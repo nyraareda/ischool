a582136a0296d03b224fc4f86a766e22026386f9
Nyraa Reda <102329189+nyraareda@users.noreply.github.com>
2024-08-02 17:19:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function handleChangeColor(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    let color = "rgb(" + red + "," + green + "," + blue + ")";
+
+    document.getElementById("flipperColor").style.backgroundColor = color;
+}
